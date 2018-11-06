7785920f55b40fd40cf783a18103feed68e4ef0b
Radwa <radwasherif3@gmail.com>
2018-11-06 16:15:23 +0200

    added camera toggle

---
+++ Assets/Scripts/CameraToggle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraToggle : MonoBehaviour {
+	public Camera top, thirdPerson; 
+	// Use this for initialization
+	void Start () {
+		top.enabled = false; 
+		thirdPerson.enabled = true; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.C)) {
+			top.enabled = !top.enabled; 
+			thirdPerson.enabled = !thirdPerson.enabled; 
+		}
+	}
+}
